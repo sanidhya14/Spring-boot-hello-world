847e186de71a3736a9284d50dcbb611248838b45
Sanidhya Khajuria <sanidh3@gmail.com>
2020-05-12 14:24:04 +0530

    simple Hello-World

---
+++ src/test/java/com/sanidhya/firstproject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanidhya.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
